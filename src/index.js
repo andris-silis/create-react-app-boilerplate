a88d6f0610d0719e5c53f40ac55405f25083cc1c
Andris Silis <andris.silis@gmail.com>
2017-12-11 23:02:09 +0200

    Add Redux boilerplate

--- src/index.js
+++ src/index.js
@@ -1,8 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import {
+  createStore,
+  combineReducers,
+  compose,
+  applyMiddleware,
+} from 'redux';
+// import { combineReducers } from 'redux-immutable';
+import {
+  Provider,
+} from 'react-redux';
+import thunk from 'redux-thunk';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import registerServiceWorker from './registerServiceWorker';
+import App from './App';
+import './index.css';
+
+
+const appliedMiddlewares = [thunk];
+const createStoreWithMiddleware = compose(
+  applyMiddleware(...appliedMiddlewares),
+  window.devToolsExtension ? window.devToolsExtension() : (f) => f,
+)(createStore);
+const reducer = combineReducers({
+// key: reducer function here
+});
+const store = createStoreWithMiddleware(reducer);
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
